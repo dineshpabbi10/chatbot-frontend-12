61f80bad2ac53b99fadaa29fc2cbc5369b804251
vikas583 <aroravikas583@gmail.com>
2022-01-10 01:09:37 +0530

    Login, Signup, services setup, Auth Guard setup - Done

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { CommonService } from '../../services/common.service';
+import { ToastrService } from 'ngx-toastr'
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Router, ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
@@ -7,9 +14,94 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  //Variable Declaration
+  registrationForm: any = FormGroup
+  countriesList: any
+  // submitted = false
+  dropdownSettings = {
+    singleSelection: true,
+    idField: 'code',
+    textField: 'name',
+    itemsShowLimit: 1,
+    allowSearchFilter: true
+  };
+
+  constructor(private formBuilder: FormBuilder, private CommonService: CommonService, private toastr: ToastrService, private ngxService: NgxUiLoaderService, public router: Router) { }
 
   ngOnInit(): void {
+
+    this.registrationForm = this.formBuilder.group({
+      first_name: new FormControl(null, [Validators.required, Validators.pattern('[a-zA-Z ]*')]),
+      last_name: new FormControl(null, [Validators.required, Validators.pattern('[a-zA-Z ]*')]),
+      mobile_no: new FormControl(null, [Validators.required, Validators.pattern('[0-9]*')]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required]),
+      confirm_password: new FormControl(null, [Validators.required]),
+      country_code: new FormControl(null, [Validators.required]),
+      agree_on_terms: new FormControl(null, [Validators.required])
+    }, {
+      validator: MustMatch('password', 'confirm_password')
+    });
+
+    this.getCountries()
   }
 
+  registerUser() {
+    this.ngxService.start()
+    // this.submitted = true
+    if (this.registrationForm.status == "INVALID") {
+      this.toastr.warning('All fields are mandatory', 'Warning')
+      this.ngxService.stop()
+      return
+    }
+    this.CommonService.signup(this.registrationForm.value).subscribe(data => {
+      if (data.status) {
+        this.toastr.success(data.message, 'SUCCESS')
+        this.router.navigate(['/'])
+
+      }
+      else {
+        this.toastr.error(data.message, 'ERROR')
+      }
+      this.ngxService.stop()
+    })
+    // console.log(this.registrationForm)
+  }
+
+  getCountries() {
+    this.ngxService.start()
+    this.CommonService.countriesList().subscribe(data => {
+
+      // this.toastr.success(data.message, 'SUCCESS')
+      this.countriesList = data.data
+
+      this.ngxService.stop()
+    })
+  }
+
+
+  onItemSelect(evt: any) {
+
+  }
+
+}
+
+
+export function MustMatch(controlName: string, matchingControlName: string) {
+  return (formGroup: FormGroup) => {
+    const control = formGroup.controls[controlName];
+    const matchingControl = formGroup.controls[matchingControlName];
+
+    if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+      // return if another validator has already found an error on the matchingControl
+      return;
+    }
+
+    // set error on matchingControl if validation fails
+    if (control.value !== matchingControl.value) {
+      matchingControl.setErrors({ mustMatch: true });
+    } else {
+      matchingControl.setErrors(null);
+    }
+  }
 }
---
+++ src/app/services/common.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpEventType, HttpRequest, HttpErrorResponse, HttpEvent } from '@angular/common/http';
+import { environment } from '../../environments/environment.prod';
+import { Observable, throwError } from 'rxjs';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommonService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      // 'Content-Type': 'application/json'
+    })
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
+
+  signup(data: any): Observable<any> {
+    return this.httpClient.post<any>(environment.endPoint + "registration/company/", data)
+      .pipe()
+  }
+
+  login(data: any): Observable<any> {
+    return this.httpClient.post<any>(environment.endPoint + "login", data).pipe()
+  }
+
+  countriesList(): Observable<any> {
+    return this.httpClient.get<any>(environment.endPoint + "api/country").pipe()
+  }
+}
