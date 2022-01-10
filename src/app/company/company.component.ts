ab55ec5c0e1974c1c5345700d183466cb4254fcc
Dinesh Pabbi <dineshpabbi10@gmail.com>
2022-01-09 16:03:38 -0800

    Add titlebar

--- src/app/company/company.component.ts
+++ src/app/company/company.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-company',
@@ -8,13 +9,20 @@ import { Component, OnInit } from '@angular/core';
 export class CompanyComponent implements OnInit {
 
   public sidebarOpen = false;
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
 
   toggleSidebar(){
     this.sidebarOpen = !this.sidebarOpen;
+  }
+
+  getTitle(){
+    if(this.router.url === "/company/dashboard"){
+      return "Dashboard";
+    }
+    return "Not Found";
   }
 
 }
