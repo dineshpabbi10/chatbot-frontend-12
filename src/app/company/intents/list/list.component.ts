2fc4beacd816343e5e89d74671671d8d415a5740
Dinesh Pabbi <dineshpabbi10@gmail.com>
2022-02-07 16:15:02 -0800

    add edit popups for entity and intents

--- src/app/company/intents/list/list.component.ts
+++ src/app/company/intents/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
 import { ConfirmationService } from 'primeng/api';
@@ -13,13 +14,21 @@ import { CompanyService } from '../../services/company.service';
 })
 export class ListComponent implements OnInit {
   public intentList: any[] = [];
+  public selectedRow: any;
+  public form : FormGroup = new FormGroup({
+    newIntent : new FormControl('',[Validators.required,this.companyService.noWhitespaceValidator]),
+    newUserSay : new FormControl('',[Validators.required,this.companyService.noWhitespaceValidator]),
+    newResponse : new FormControl('',[Validators.required,this.companyService.noWhitespaceValidator])
+  });
+  public updatePayload : any = {};
+
   public cols = [
     { field: 'domain', header: 'Domain' },
     { field: 'intent', header: 'Intent' },
     { field: 'response', header: 'Response' },
     { field: 'user_say', header: 'User Response' },
   ];
-
+ 
   constructor(
     private companyService: CompanyService,
     private toast: ToastrService,
@@ -51,16 +60,25 @@ export class ListComponent implements OnInit {
       }
     });
 
+    // subscribe to edit change
+    this.form.valueChanges.subscribe((data)=>{
+      this.updatePayload = 
+        {
+          "intent": this.selectedRow?.intent,
+          "user_say": this.selectedRow?.user_say,
+          "response": this.selectedRow?.response,
+          "domain": this.selectedRow?.domain,
+          "newintent": this.form.get("newIntent")?.value,
+          "newuser_say": this.form.get("newUserSay")?.value,
+          "newresponse": this.form.get("newResponse")?.value
+        }
+    })
+    
+
     // Edit button clicked in generic table
     this.companyService.selectedRecord$.subscribe((data) => {
       if (data.component === 'intents' && data.action === 'edit') {
-        this.confirmationService.confirm({
-          message: 'Are you sure that you want to perform insert action?',
-          accept: () => {
-            //Actual logic to perform a confirmation
-            console.log(data);
-          },
-        });
+        this.selectedRow = data.payload[0];
       }
     });
 
