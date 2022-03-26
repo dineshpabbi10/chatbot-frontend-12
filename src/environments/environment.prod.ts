3a3a832f564cc9d97c0e8d5abc379c4f989150f6
Dinesh Pabbi <dineshpabbi10@gmail.com>
2022-03-26 11:05:48 -0600

    add firebase env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,13 +5,23 @@ export const env = {
 
 export interface Environment {
   endPoint: string;
-  production: boolean
+  production: boolean;
+  firebase: any
 }
 
 
 export const DEV: Environment = {
   endPoint: 'http://34.131.139.183:8000/',
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB0jFiFcMCEZJiRxfoabVHRo_6yCVAHfWo",
+    authDomain: "foodhunt-20680.firebaseapp.com",
+    projectId: "foodhunt-20680",
+    storageBucket: "foodhunt-20680.appspot.com",
+    messagingSenderId: "744761034195",
+    appId: "1:744761034195:web:36d35935e5702afc9f1b26",
+    measurementId: "G-ZDMMWW0756"
+  }
 }
 
 export const environment: Environment = DEV;
