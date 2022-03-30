f320bef13c61f0fa62f5ef0f810485ec9a039e87
Dinesh Pabbi <dineshpabbi10@gmail.com>
2022-03-29 23:57:26 -0600

    add profile update

--- src/app/company/profile/profile.module.ts
+++ src/app/company/profile/profile.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
+import { CommonImport } from 'src/app/common.imports';
 
 
 @NgModule({
@@ -10,6 +11,7 @@ import { ProfileComponent } from './profile.component';
   ],
   imports: [
     CommonModule,
+    CommonImport,
     ProfileRoutingModule
   ]
 })
