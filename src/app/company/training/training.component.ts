ecd75c15919d938fbe8853891ce39bb8fc58b52e
Dinesh Pabbi <dineshpabbi10@gmail.com>
2022-04-19 22:56:35 -0600

    complete training

--- src/app/company/training/training.component.ts
+++ src/app/company/training/training.component.ts
@@ -11,95 +11,140 @@ import { CompanyService } from '../services/company.service';
 @Component({
   selector: 'app-training',
   templateUrl: './training.component.html',
-  styleUrls: ['./training.component.css']
+  styleUrls: ['./training.component.css'],
 })
 export class TrainingComponent implements OnInit {
-  public trainingTitle = "";
+  public trainingTitle = '';
   public isTraining = false;
-  public domainToken : any  = null;
-  public SOCKET_URL = 'wss://34.131.139.183:4444/ws/chatroom/';
+  public domainToken: any = null;
+  public userId: any = null;
+  public SOCKET_URL_BASE = 'wss://34.131.139.183:4444/ws/chatroom/';
+  public SOCKET_URL = '';
   public cols = [
     { field: 'datetime', header: 'Training Date' },
     { field: 'status', header: 'Training Status' },
   ];
-  public trainingHistory :any[] = []
+  public trainingHistory: any[] = [];
 
-  constructor(private companyService: CompanyService,private companySocketService: CompanySocketService, private toaster : ToastrService, private loader : NgxUiLoaderService) { }
+  constructor(
+    private companyService: CompanyService,
+    private companySocketService: CompanySocketService,
+    private toaster: ToastrService,
+    private loader: NgxUiLoaderService
+  ) {}
 
   ngOnInit(): void {
-    this.trainingTitle = "All Trainings Complete";
-    this.companySocketService.openWebSocketConnection(this.SOCKET_URL+'dineshpabbi10@gmail.com');
+    // create socket on user detail fetch
+    this.companyService
+      .getCompanyUserDetails()
+      .pipe(
+        catchError((err) => {
+          this.toaster.error(err.message);
+          return of(err.message);
+        })
+      )
+      .subscribe((res) => {
+        if (res.status) {
+          this.userId = res.data.userID;
+          this.SOCKET_URL = this.SOCKET_URL_BASE + this.userId + '/'
+          this.companySocketService.openWebSocketConnection(
+            this.SOCKET_URL
+          );
+        }
+      });
 
-    this.companySocketService.socketCloseSubject$.subscribe((data:any)=>{
-      setTimeout(()=>{
-        this.getTrainingStatusFromSocket();
-      },1000)
+    this.companySocketService.socketCloseSubject$.subscribe((error) => {
+      console.log('RECONNECTING');
+      setTimeout(() => {
+        this.companySocketService.openWebSocketConnection(this.SOCKET_URL);
+      }, 10000);
     });
 
-    this.companySocketService.socketResponseSubject$.subscribe((data:any)=>{
-      console.log(data);
+    // listen to socket messages
+    this.companySocketService.socketResponseSubject$.subscribe((message:any)=>{
+      if(message.type === "train_status"){
+        if(message.payload.msg === "Training started"){
+          this.trainingTitle = "Training in progress";
+          this.isTraining = true;
+          this.getTrainHistory();
+        }else{
+          this.trainingTitle = "All trainings complete";
+          this.isTraining = false;
+          this.getTrainHistory();
+        }
+      }
     })
 
+    this.companySocketService.socketCloseSubject$.subscribe((data: any) => {
+      setTimeout(() => {
+        this.getTrainingStatusFromSocket();
+      }, 1000);
+    });
 
-    
+    this.companySocketService.socketResponseSubject$.subscribe((data: any) => {
+      console.log(data);
+    });
+
     this.getDomains();
 
     this.getTrainHistory();
   }
 
-  getDomains(){
+  getDomains() {
     this.loader.start();
     this.companyService
-    .getWebsiteTokens()
-    .pipe(
-      catchError((err) => {
-        this.toaster.error(err.message);
-        return of(err.message);
-      })
-    )
-    .subscribe((res) => {
-      if (res.status) {
-        this.domainToken = res.data[0]?.token;
-      }
-      this.loader.stop();
-    });
+      .getWebsiteTokens()
+      .pipe(
+        catchError((err) => {
+          this.toaster.error(err.message);
+          return of(err.message);
+        })
+      )
+      .subscribe((res) => {
+        if (res.status) {
+          this.domainToken = res.data[0]?.token;
+        }
+        this.loader.stop();
+      });
   }
 
-  getTrainHistory(){
+  getTrainHistory() {
     this.loader.start();
     this.companyService
-    .getTrainHistory()
-    .pipe(
-      catchError((err) => {
-        this.toaster.error(err.message);
-        return of(err.message);
-      })
-    )
-    .subscribe((res) => {
-      if (res.status) {
-        this.trainingHistory = res.data;
-      }
-      this.loader.stop();
-    });
+      .getTrainHistory()
+      .pipe(
+        catchError((err) => {
+          this.toaster.error(err.message);
+          return of(err.message);
+        })
+      )
+      .subscribe((res) => {
+        if (res.status) {
+          this.trainingHistory = res.data.history;
+          this.isTraining = res.data.train_status;
+          this.trainingTitle = this.isTraining ? "Training in progress" : "All trainings complete";
+        }
+        this.loader.stop();
+      });
   }
 
-  startTraining(){
+  startTraining() {
     this.loader.start();
     this.companyService
-    .trainBot(this.domainToken)
-    .pipe(
-      catchError((err) => {
-        this.toaster.error(err.message);
-        return of(err.message);
-      })
-    )
-    .subscribe((res) => {
-      if (res.status) {
-        this.toaster.success("Training started successfully");
-        this.getTrainHistory();
-      }
-      this.loader.stop();
-    });
+      .trainBot(this.domainToken)
+      .pipe(
+        catchError((err) => {
+          this.toaster.error(err.message);
+          return of(err.message);
+        })
+      )
+      .subscribe((res) => {
+        if (res.status) {
+          this.toaster.success('Training started successfully');
+          this.getTrainHistory();
+        }
+        this.loader.stop();
+      });
   }
 
   getTrainingStatusFromSocket() {
@@ -114,7 +159,4 @@ export class TrainingComponent implements OnInit {
     console.log('GETTING MESSAGE');
     this.companySocketService.sendWebSocketMessage(data);
   }
-
-  
-
 }
