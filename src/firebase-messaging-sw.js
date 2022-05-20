664e3bcd7e3a732f05f77d88a393aca7ec119d11
Dinesh Pabbi <dineshpabbi10@gmail.com>
2022-05-20 09:22:31 -0600

    fix sidebar button

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,20 +1,20 @@
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/8.1.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.1.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
 
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 
 firebase.initializeApp({
-    apiKey: "AIzaSyB0jFiFcMCEZJiRxfoabVHRo_6yCVAHfWo",
-    authDomain: "foodhunt-20680.firebaseapp.com",
-    projectId: "foodhunt-20680",
-    storageBucket: "foodhunt-20680.appspot.com",
-    messagingSenderId: "744761034195",
-    appId: "1:744761034195:web:36d35935e5702afc9f1b26",
-    measurementId: "G-ZDMMWW0756"
+    apiKey: "AIzaSyD6_cEhjYmGtA5XoW3PhPQaoFqUEEZw99Q",
+    authDomain: "elasbotchatbot.firebaseapp.com",
+    projectId: "elasbotchatbot",
+    storageBucket: "elasbotchatbot.appspot.com",
+    messagingSenderId: "742241989692",
+    appId: "1:742241989692:web:3cdc0f927256be516b3f39",
+    measurementId: "G-C8LYF79RSR"
   });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
